5f29d66b04ab7ed93770af4c9895fbe798ef1a2c
Federico David Roviralta Terrón <david_roviralta@hotmail.com>
2022-08-01 21:45:27 +0100

    Google Test Examples

---
+++ GTTests/src/GTTests.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : GTTests.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <gtest/gtest.h>
+
+TEST(FooTest, tests_foo){
+	ASSERT_TRUE(true);
+}
